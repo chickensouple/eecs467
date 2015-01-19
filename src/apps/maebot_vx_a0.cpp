1b4ae33c03e1e60b7931f5516c16abff9ac5ef08
Clark Zhang <blastpower2@gmail.com>
2015-01-19 00:23:43 -0500

    vx work, doing odometry

--- src/apps/maebot_vx_a0.cpp
+++ src/apps/maebot_vx_a0.cpp
@@ -4,13 +4,15 @@
 #include <math.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <lcm/lcm.h>
+
+#include <vector>
 
 // core api
 #include "vx/vx.h"
 #include "vx/vx_util.h"
 
 #include "vx/gtk/vx_gtk_display_source.h"
-#include "vx/vx_remote_display_source.h"
 
 // drawables
 #include "vx/vxo_drawables.h"
@@ -19,8 +21,24 @@
 #include "imagesource/image_u32.h"
 #include "imagesource/image_util.h"
 
+#include "lcmtypes/maebot_motor_feedback_t.h"
+#include "lcmtypes/maebot_a0_sensor_data_t.h"
+#include "lcmtypes/maebot_sensor_data_t.h"
+
+#define BASE_LENGTH 0.08 // meters
+#define METERS_PER_TICK 0.002083333333 // meters
+
 typedef struct
 {
+	lcm_t* lcm;
+	pthread_mutex_t lcm_mutex;
+
+
+	bool first_odo;
+	double odo_x_curr, odo_y_curr, odo_heading_curr;
+	int odo_left_tick, odo_right_tick;
+	std::vector<float> odo_points;
+
 	int running;
 
 	image_u32_t *img;
@@ -34,6 +52,69 @@ typedef struct
 	pthread_t animate_thread;
 } state_t;
 
+state_t * state;
+
+void* lcm_handle_thread(void* arg) {
+	while(1) {
+		pthread_mutex_lock(&state->lcm_mutex);
+		lcm_handle(state->lcm);
+		pthread_mutex_unlock(&state->lcm_mutex);
+		usleep(1000);
+	}
+
+	return NULL;
+}
+
+static void sensor_data_handler(const lcm_recv_buf_t* rbuf,
+	const char* channel,
+	const maebot_sensor_data_t *msg, void* user) {
+
+}
+
+static void a0_sensor_data_handler(const lcm_recv_buf_t* rbuf,
+	const char* channel,
+	const maebot_a0_sensor_data_t *msg, void* user) {
+
+}
+
+static void motor_feedback_handler(const lcm_recv_buf_t *rbuf,
+	const char *channel,
+	const maebot_motor_feedback_t *msg, void *user)
+{
+	if (state->first_odo) {
+		state->odo_left_tick = msg->encoder_left_ticks;
+		state->odo_right_tick = msg->encoder_right_ticks;
+		state->first_odo = false;
+		return;
+	}
+
+
+	double deltaLeft = msg->encoder_left_ticks - state->odo_left_tick;
+	double deltaRight = msg->encoder_right_ticks - state->odo_right_tick;
+	state->odo_left_tick = msg->encoder_left_ticks;
+	state->odo_right_tick = msg->encoder_right_ticks;
+
+	deltaLeft *= METERS_PER_TICK;
+	deltaRight *= METERS_PER_TICK;
+
+	double distance = (deltaRight + deltaLeft) / 2;
+	double theta = (deltaRight - deltaLeft) / BASE_LENGTH;
+	double alpha = theta / 2;
+	state->odo_x_curr += distance * cos(state->odo_heading_curr + alpha);
+	state->odo_y_curr += distance * sin(state->odo_heading_curr + alpha);
+	state->odo_heading_curr += theta;
+	// printf("%lf,\t%lf\n", theta, state->odo_heading_curr);
+	printf("%lf,\t%lf\n", state->odo_x_curr, state->odo_y_curr);
+
+
+	state->odo_points.push_back(state->odo_x_curr);
+	state->odo_points.push_back(state->odo_y_curr);
+	state->odo_points.push_back(0);
+	int npoints = state->odo_points.size();
+	vx_resc_t *verts = vx_resc_copyf((state->odo_points).data(), npoints);
+	vx_buffer_add_back(vx_world_get_buffer(state->world,"test"), vxo_lines(verts, npoints, GL_LINES, vxo_points_style(vx_red, 2.0f)));
+	vx_buffer_swap(vx_world_get_buffer(state->world,"test"));
+}
 
 
 static void draw(state_t * state, vx_world_t * world)
@@ -105,6 +186,7 @@ static void state_destroy(state_t * state)
 	free(state);
 
 	pthread_mutex_destroy(&state->mutex);
+	lcm_destroy(state->lcm);
 
 }
 
@@ -116,6 +198,21 @@ static state_t * state_create()
 	state->app.display_started=display_started;
 	state->app.display_finished=display_finished;
 
+	state->lcm = lcm_create(NULL);
+	if (!state->lcm) {
+		printf("lcm create failed\n");
+		exit(1);
+	}
+
+	if (pthread_mutex_init(&state->lcm_mutex, NULL)) {
+		printf("lcm mutex init failed\n");
+		exit(1);
+	}
+
+	state->first_odo = true;
+	state->odo_y_curr = 0;
+	state->odo_x_curr = 0;
+	state->odo_heading_curr = 0;
 
 	state->world = vx_world_create();
 	state->layers = zhash_create(sizeof(vx_display_t*), sizeof(vx_layer_t*), zhash_ptr_hash, zhash_ptr_equals);
@@ -174,12 +271,14 @@ int main(int argc, char ** argv)
 {
 	vx_global_init(); // Call this to initialize the vx-wide lock. Required to start the GL thread or to use the program library
 
-	state_t * state = state_create();
+	state = state_create();
 
+	// draw(state, state->world);
 
-	draw(state, state->world);
+	// pthread_create(&state->animate_thread, NULL, render_loop, state);
 
-	pthread_create(&state->animate_thread, NULL, render_loop, state);
+	pthread_t lcm_handle_thread_pid;
+	pthread_create(&lcm_handle_thread_pid, NULL, lcm_handle_thread, NULL);
 
 	gdk_threads_init ();
 	gdk_threads_enter ();
@@ -196,6 +295,22 @@ int main(int argc, char ** argv)
 
 	g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+
+	maebot_motor_feedback_t_subscribe(state->lcm,
+		"MAEBOT_MOTOR_FEEDBACK",
+		motor_feedback_handler,
+		NULL);
+
+	maebot_sensor_data_t_subscribe(state->lcm,
+		"MAEBOT_SENSOR_DATA",
+		sensor_data_handler,
+		NULL);
+
+	maebot_a0_sensor_data_t_subscribe(state->lcm,
+		"MAEBOT_A0_SENSOR_DATA",
+		a0_sensor_data_handler,
+		NULL);
+
 	gtk_main (); // Blocks as long as GTK window is open
 	gdk_threads_leave ();
 
@@ -203,7 +318,6 @@ int main(int argc, char ** argv)
 
 
 	pthread_join(state->animate_thread, NULL);
-	// vx_remote_display_source_destroy(cxn);
 
 	state_destroy(state);
 	vx_global_destroy();
